201fafdae8883fdfd5d3aa980a9ce58be99dd14e
GaoKangYu <13617240218@163.com>
2021-04-16 15:19:22 +0800

    Add files via upload

---
+++ 第77次失败/printNumbers/printNumbers/printNumbers.cpp
@@ -0,0 +1,46 @@
+﻿/*
+剑指 Offer 17. 打印从1到最大的n位数
+
+输入数字 n，按顺序打印出从 1 到最大的 n 位十进制数。比如输入 3，则打印出 1、2、3 一直到最大的 3 位数 999。
+
+示例 1:
+
+输入: n = 1
+输出: [1,2,3,4,5,6,7,8,9]
+ 
+
+说明：
+用返回一个整数列表来代替打印
+n 为正整数
+*/
+
+/*
+n位，那，在于如何判定结束
+位运算？
+
+...
+这题目改的太简单了
+现在看来三周目很有必要了
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<int> printNumbers(int n) {
+		vector<int> ret;
+		int val = 1;
+		int max = pow(10, n) - 1;
+		while (val < max){
+			ret.push_back(++val);
+		}
+		return ret;
+	}
+};
+
+int main() {
+	std::cout << "Hello World!\n";
+}
