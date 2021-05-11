c2bfbb3be6c955882fc0b0181087721f224a496e
GaoKangYu <13617240218@163.com>
2021-05-11 11:18:43 +0800

    Add files via upload

---
+++ 已经没有什么好怕的了/sumNums.cpp
@@ -0,0 +1,102 @@
+﻿/*
+Day2
+剑指 Offer 64. 求1+2+…+n
+
+求 1+2+...+n ，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
+
+示例 1：
+输入: n = 3
+输出: 6
+
+示例 2：
+输入: n = 9
+输出: 45
+ 
+
+限制：
+1 <= n <= 10000
+*/
+
+/*
+快速乘
+
+求和即为n(n+1)/2
+计算n+1这个因子对乘法的贡献度，二进制展开，第i个为1的位贡献了2^i-1，累加即可
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	int sumNums(int n) {
+		int ans = 0;
+		int A = n;
+		int B = n + 1;
+
+		(B & 1) && (ans += A);
+		B >>= 1;
+		A <<= 1;
+
+		(B & 1) && (ans += A);
+		B >>= 1;
+		A <<= 1;
+
+		(B & 1) && (ans += A);
+		B >>= 1;
+		A <<= 1;
+
+		(B & 1) && (ans += A);
+		B >>= 1;
+		A <<= 1;
+
+		(B & 1) && (ans += A);
+		B >>= 1;
+		A <<= 1;
+
+		(B & 1) && (ans += A);
+		B >>= 1;
+		A <<= 1;
+
+		(B & 1) && (ans += A);
+		B >>= 1;
+		A <<= 1;
+
+		(B & 1) && (ans += A);
+		B >>= 1;
+		A <<= 1;
+
+		(B & 1) && (ans += A);
+		B >>= 1;
+		A <<= 1;
+
+		(B & 1) && (ans += A);
+		B >>= 1;
+		A <<= 1;
+
+		(B & 1) && (ans += A);
+		B >>= 1;
+		A <<= 1;
+
+		(B & 1) && (ans += A);
+		B >>= 1;
+		A <<= 1;
+
+		(B & 1) && (ans += A);
+		B >>= 1;
+		A <<= 1;
+
+		(B & 1) && (ans += A);
+		B >>= 1;
+		A <<= 1;
+
+		return ans >> 1;
+	}
+};
+
+int main() {
+	Solution solu;
+	cout << solu.sumNums(13) << endl;
+}
