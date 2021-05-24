5f552a81de236ba606e435621cf5834394636bca
GaoKangYu <13617240218@163.com>
2021-05-24 16:42:52 +0800

    Add files via upload

---
+++ 已经没有什么好怕的了/deleteNode.cpp
@@ -0,0 +1,84 @@
+﻿/*
+Day2
+剑指 Offer 18. 删除链表的节点
+
+给定单向链表的头指针和一个要删除的节点的值，定义一个函数删除该节点。
+
+返回删除后的链表的头节点。
+
+注意：此题对比原题有改动
+
+示例 1:
+输入: head = [4,5,1,9], val = 5
+输出: [4,1,9]
+解释: 给定你链表中值为 5 的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+
+示例 2:
+输入: head = [4,5,1,9], val = 1
+输出: [4,5,9]
+解释: 给定你链表中值为 1 的第三个节点，那么在调用了你的函数之后，该链表应变为 4 -> 5 -> 9.
+ 
+
+说明：
+题目保证链表中节点的值互不相同
+若使用 C 或 C++ 语言，你不需要 free 或 delete 被删除的节点
+*/
+
+/*
+思路
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct ListNode{
+	int val;
+	ListNode* next;
+	ListNode(int x): val(x), next(NULL){ }
+	ListNode() : val(0), next(NULL) { }
+};
+
+class Solution {
+public:
+	ListNode* deleteNode(ListNode* head, int val) {
+		ListNode* ret = head;
+		ListNode* pre = NULL;
+		if (head->val == val) {
+			ret = head->next;
+			delete head;
+			return ret;
+		}
+		while (head) {
+			ListNode* next = head->next;
+			if (head->val == val) {
+				delete head;
+				pre->next = next;
+				break;
+			}
+			pre = head;
+			head = head->next;
+		}
+		return ret;
+	}
+};
+
+int main() {
+	ListNode* head = new ListNode(1);
+	ListNode* node_1 = new ListNode(2);
+	ListNode* node_2 = new ListNode(3);
+	ListNode* node_3 = new ListNode(4);
+
+	head->next = node_1;
+	node_1->next = node_2;
+	node_2->next = node_3;
+
+	Solution solu;
+	solu.deleteNode(head, 3);
+	while (head){
+		cout << head->val << " ";
+		head = head->next;
+	}
+	cout << endl;
+}
