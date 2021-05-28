23dc3c2e44005473faac52a80e360925e975df30
GaoKangYu <13617240218@163.com>
2021-05-28 11:32:00 +0800

    Add files via upload

---
+++ 已经没有什么好怕的了/pathSum.cpp
@@ -0,0 +1,50 @@
+﻿/*
+Day2
+题目描述
+*/
+
+/*
+思路
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode{
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL){ }
+};
+
+class Solution {
+public:
+	vector<vector<int>> pathSum(TreeNode* root, int target) {
+		dfs(root, target);
+		return ret;
+	}
+
+	void dfs(TreeNode* root, int target) {
+		if (!root) {
+			return;
+		}
+		path.emplace_back(root->val);
+		target -= root->val;
+		if (!root->left && !root->right && !target) {
+			ret.emplace_back(path);
+		}
+		dfs(root->left, target);
+		dfs(root->right, target);
+		path.pop_back();
+	}
+
+private:
+	vector<vector<int> > ret;
+	vector<int> path;
+};
+
+int main() {
+
+}
