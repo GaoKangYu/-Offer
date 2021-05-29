57b774a159634f10a94fbdf43cf156925dc1114a
GaoKangYu <13617240218@163.com>
2021-05-29 16:10:47 +0800

    Add files via upload

---
+++ 已经没有什么好怕的了/lowestCommonAncestor_II.cpp
@@ -0,0 +1,50 @@
+﻿/*
+Day2
+剑指 Offer 68 - II. 二叉树的最近公共祖先
+
+给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+*/
+
+/*
+思路
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode{
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL){ }
+};
+
+class Solution {
+public:
+	TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+		if (root == NULL) {
+			return NULL;
+		}
+		if (q == root || p == root) {
+			return root;
+		}
+		TreeNode* left = lowestCommonAncestor(root->left, p, q);
+		TreeNode* right = lowestCommonAncestor(root->right, p, q);
+		if (left != NULL && right != NULL) {
+			return root;
+		}
+		if (left != NULL) {
+			return left;
+		}
+		if (right != NULL) {
+			return right;
+		}
+		return NULL;
+	}
+};
+
+int main() {
+
+}
