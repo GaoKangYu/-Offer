40613c2ec1953d31951867a92c2139e19481269d
GaoKangYu <13617240218@163.com>
2021-06-02 09:00:55 +0800

    Add files via upload

---
+++ 勇气与爱与奇迹和魔法/countDigitOne.cpp
@@ -0,0 +1,82 @@
+﻿/*
+Day3
+剑指 Offer 43. 1～n 整数中 1 出现的次数
+
+输入一个整数 n ，求1～n这n个整数的十进制表示中1出现的次数。
+例如，输入12，1～12这些整数中包含1 的数字有1、10、11和12，1一共出现了5次。
+
+ 
+示例 1：
+输入：n = 12
+输出：5
+
+示例 2：
+输入：n = 13
+输出：6 
+
+限制：
+1 <= n < 2^31
+*/
+
+/*
+0 : high * i + 0
+1 : high * i + i + 1
+other : high * i + i
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	int countDigitOne(int n) {
+		//return value count, for counting amount of one
+		int count = 0;
+		//position of current pointer
+		long i = 1;
+		//current pos
+		while (n / i) {
+			//1532
+			//1532 / 10
+			//1532 / 100
+			//1532 / 1000
+			//n / i * 10
+			//left pos of cur
+			int high = n / (i * 10);
+			//cur pos
+			//1532  2 1532 % 10
+			//1532  3 1532 / 10 % 10
+			//1532  5 1532 / 100 % 10
+			//1532  1
+			// n / i
+			int cur = n / i % 10;
+			//right pos of cur
+			//1532  0 1532 % i
+			//1532  2 
+			//1532  3
+			//1532  5
+			//1532  1
+			int low = n % i;
+			if (cur == 0) {
+				//1502
+				count += high * i;
+			}
+			else if (cur == 1) {
+				//1512
+				count += high * i + low + 1;
+			}
+			else {
+				//1522
+				count += high * i + i;
+			}
+			i *= 10;
+		}
+		return count;
+	}
+};
+
+int main() {
+
+}
---
+++ 勇气与爱与奇迹和魔法/treeToDoublyList.cpp
@@ -0,0 +1,89 @@
+﻿/*
+Day3
+剑指 Offer 36. 二叉搜索树与双向链表
+
+输入一棵二叉搜索树，将该二叉搜索树转换成一个排序的循环双向链表。
+要求不能创建任何新的节点，只能调整树中节点指针的指向。
+*/
+
+/*
+着眼双向链表
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Node{
+	int val;
+	Node* left;
+	Node* right;
+
+	Node(){ }
+	Node(int _val) : val(_val), left(NULL), right(NULL){ }
+	Node(int _val, Node* _left, Node* _right) : val(_val), left(_left), right(_right){ }
+};
+
+class Solution {
+public:
+	Node* treeToDoublyList(Node* root) {
+		if (!root) {
+			return NULL;
+		}
+		treeToDoublyListCore(root);
+		//首尾相连
+		head->left = pre;
+		pre->right = head;
+		return head;
+	}
+
+	void treeToDoublyListCore(Node* cur) {
+		if (!cur) {
+			return;
+		}
+		//中序遍历
+		//实际的逻辑是，当前节点的左指针指向前一节点，前一节点的右指针指向当前节点
+		treeToDoublyListCore(cur->left);
+		if (pre != NULL) {
+			pre->right = cur;
+			cur->left = pre;
+		}
+		//如果找到了头结点，直接赋值给头结点
+		else {
+			head = cur;
+		}
+		pre = cur;
+		treeToDoublyListCore(cur->right);
+	}
+
+	void showResult(Node* input) {
+
+	}
+
+private:
+	Node* pre = new Node();
+	Node* head = new Node();
+};
+
+int main() {
+	Node* head = new Node(4);
+	Node* _1_left = new Node(2);
+	Node* _1_right = new Node(5);
+	Node* _2_left = new Node(1);
+	Node* _2_right = new Node(3);
+
+	head->left = _1_left;
+	head->right = _1_right;
+	_1_left->left = _2_left;
+	_1_left->right = _2_right;
+
+	Solution solu;
+	solu.treeToDoublyList(input);
+	head = head->right;
+	while (true) {
+		cout << head->val << " ";
+		head = head->right;
+	}
+	cout << endl;
+}
