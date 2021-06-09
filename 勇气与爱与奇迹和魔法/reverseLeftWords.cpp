8f7e268f9f1036b18eb6e7a2aef39f44f6fcaba9
GaoKangYu <13617240218@163.com>
2021-06-09 15:57:56 +0800

    Add files via upload

---
+++ 勇气与爱与奇迹和魔法/reverseLeftWords.cpp
@@ -0,0 +1,46 @@
+﻿/*
+Day3
+题目描述
+*/
+
+/*
+b bn
+bn e
+b e
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	string reverseLeftWords(string s, int n) {
+		reverse(s.begin(), s.begin() + n);
+		reverse(s.begin() + n, s.end());
+		reverse(s.begin(), s.end());
+		return s;
+	}
+};
+
+
+class Solution_v1 {
+public:
+	string reverseLeftWords(string s, int n) {
+		string ret;
+		int i = n;
+		for (; i < s.size(); ++i) {
+			ret += s[i];
+		}
+		i = 0;
+		for (; i < n; ++i) {
+			ret += s[i];
+		}
+		return ret;
+	}
+};
+
+int main() {
+
+}
