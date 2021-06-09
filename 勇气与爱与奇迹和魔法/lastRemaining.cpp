b3511fdded56836dabd9ebd033fe0e8b392c3889
GaoKangYu <13617240218@163.com>
2021-06-09 16:35:21 +0800

    Add files via upload

---
+++ 勇气与爱与奇迹和魔法/lastRemaining.cpp
@@ -0,0 +1,29 @@
+﻿/*
+Day3
+题目描述
+*/
+
+/*
+思路
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	int lastRemaining(int n, int m) {
+		int ans = 0;
+		for (int i = 2; i <= n; ++i){
+			ans = (ans + m) % i;
+		}
+		return ans;
+	}
+};
+
+int main() {
+	Solution solu;
+	cout << solu.lastRemaining(12, 3) << endl;
+}
